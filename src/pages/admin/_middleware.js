550dad7ab7b2dbfee76033dbf4cdbea7cd9b39cf
Eduardo Chiaro <e.chiaro@gmail.com>
2022-02-20 11:56:38 -0800

    middleware for admin pages

---
+++ src/pages/admin/_middleware.js
@@ -0,0 +1,9 @@
+import { getToken } from "next-auth/jwt"
+import { NextResponse } from "next/server"
+
+export async function middleware(req) {
+  const session = await getToken({ req, secret: process.env.SECRET })
+  console.log(session);
+  if (!session) return NextResponse.redirect(new URL('/auth/signin', req.url));
+  return NextResponse.next();
+}
