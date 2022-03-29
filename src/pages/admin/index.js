e6e423e3755c527a735be3960b05e60c93d010d2
Eduardo Chiaro <e.chiaro@gmail.com>
2022-03-29 09:34:21 -0700

    add page title

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,4 +1,5 @@
 import { useSession } from "next-auth/react"
+import Head from "next/head";
 import AdminWrapper from "../../elements/admin/Wrapper";
 
 const AdminIndex = () => {
@@ -6,6 +7,14 @@ const AdminIndex = () => {
   if (session) {
     return (
       <AdminWrapper>
+        <Head>
+          <title>Eduardo Chiaro - Admin</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <meta name="description" content="Eduardo Chiaro - Software Developer" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        </Head>
         <h1>Homepage</h1>
       </AdminWrapper>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,19 @@ import Top from '../components/Top'
 import Apps from '../components/Apps'
 import GitHub from '../components/GitHub'
 import Footer from '../components/Footer'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Eduardo Chiaro</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Eduardo Chiaro - Software Developer" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+      </Head>
       <Header />
       <Bio />
       <Jobs />
