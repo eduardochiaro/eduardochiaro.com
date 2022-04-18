ced9b111c75a5dee85ef21deab511aedb32fb791
Eduardo Chiaro <e.chiaro@gmail.com>
2022-04-18 10:01:15 -0700

    skill redesign

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import useStaleSWR from '../lib/staleSWR';
-import Gauge from '../elements/Gauge';
+import styles from '../styles/Skills.module.scss'
+import SVG from 'react-inlinesvg';
 
 export default function Skills () {
   const { data, error } = useStaleSWR('/api/portfolio/skills');
@@ -9,26 +10,25 @@ export default function Skills () {
     <section className={`px-8 lg:px-0 my-10`}>
       <span className="anchor" name="skills"/>
       <div className="max-w-5xl mx-auto">
-        <h3 className="font-header leading-tight text-2xl lg:text-3xl pr-4 font-light">
+        <h3 className="font-header leading-tight text-2xl lg:text-3xl pr-4 font-light mb-2">
           What I&apos;m <span className="text-isabelline-700">good</span> at...
         </h3>
-        <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-8 mt-4">
+        <div>
         { data && data.results ? 
             data.results.map((skill, index) => (
-              <Gauge
-                key={`g-${index}`}
-                value={skill.percentage}
-                skill={skill}
-                placeholder={false}
-                // any other options you want
-              />
-          )) : [
-              ...Array(6)
-                .fill()
-                .map((_, idx) => 0 + idx),
-            ].map((x) => (
-              <div key={x} className="w-auto mx-5 h-full bg-green-sheen-300 rounded-md animate-pulse"></div>
-            ))}
+            <div className="flex items-center mt-1">
+              <span className="flex-none font-medium text-gray-1000 dark:text-gray-100 mr-5">{skill.name}</span>
+              <span className="w-full border-t border-isabelline-600 dark:border-isabelline-800 border-dashed shrink mr-5"></span>
+              <div className="flex-none relative text-white">
+                <SVG title="Eduardo Chiaro" alt="Eduardo Chiaro" className={`inline-block h-8 w-8 fill-zinc-700 dark:fill-zinc-200`} src={`/images/svg-icons/${skill.logo}`} />
+              </div>
+              <div className="flex-none w-6/12">
+                <div className="w-full bg-zinc-200 dark:bg-zinc-700 h-4">
+                  <div className={`${styles[`bar-${skill.type}`]} bg-blue-600 h-4`} style={{width: skill.percentage + '%'}}></div>
+                </div>
+              </div>
+            </div>
+          )) : '' }
         </div>
       </div>
     </section>
