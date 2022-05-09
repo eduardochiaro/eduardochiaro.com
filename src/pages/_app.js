25c767571f35367603bc56218f74e780decc1a72
Eduardo Chiaro <e.chiaro@gmail.com>
2022-05-09 09:46:44 -0700

    favicon update

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,6 @@ import { ThemeProvider } from "next-themes";
 import NextProgress from "next-progress";
 import '../styles/globals.scss'
 import Head from "next/head";
-import tailwind from '../../tailwind.config';
 
 function MyApp({
   Component,
@@ -16,14 +15,6 @@ function MyApp({
         <Head>
           <title>Eduardo Chiaro</title>
           <meta name="description" content="Eduardo Chiaro - Software Developer" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color={tailwind.theme.colors.primary[800]} />
-          <meta name="msapplication-TileColor" content={tailwind.theme.colors.primary[200]} />
-          <meta name="theme-color" content={tailwind.theme.colors.zinc[100]} media="(prefers-color-scheme: light)" key="dark-system" />
-          <meta name="theme-color" content={tailwind.theme.colors.zinc[700]} media="(prefers-color-scheme: dark)" key="light-system"  />
         </Head>
         <Component {...pageProps} />
       </ThemeProvider>
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,10 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import tailwind from '../../tailwind.config';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color={tailwind.theme.colors.primary[800]} />
+          <meta name="msapplication-TileColor" content={tailwind.theme.colors.primary[200]} />
+          <meta name="theme-color" content={tailwind.theme.colors.zinc[100]} media="(prefers-color-scheme: light)" key="dark-system" />
+          <meta name="theme-color" content={tailwind.theme.colors.zinc[700]} media="(prefers-color-scheme: dark)" key="light-system"  />
           <link rel="preconnect" href="https://fonts.googleapis.com" /> 
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" /> 
           <link
