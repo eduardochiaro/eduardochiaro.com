384ae3345686fa7e98c9a5d50e1d3c858a1a94f2
Eduardo Chiaro <e.chiaro@gmail.com>
2022-05-20 08:35:30 -0700

    fetch command

--- src/utils/projects/terminal/commands.js
+++ src/utils/projects/terminal/commands.js
@@ -38,3 +38,45 @@ export const logo = () => {
                            @@@@@@       
                           @@@`;
 }
+
+export const fetch = () => {
+  return (
+    <div className="ml-6 mt-6">
+      <div className="border-b border-dashed  w-1/3 mb-2">
+        user@terminal.eduardochiaro.com
+      </div>
+      <div className="flex w-1/3">
+        <span className="grow text-right">OS</span>
+        <span className="mr-2">:</span>
+        <span className="text-zinc-100 w-2/3">FakeOS 5.0</span>
+      </div>
+      <div className="flex w-1/3">
+        <span className="grow text-right">Host</span>
+        <span className="mr-2">:</span>
+        <span className="text-zinc-100 w-2/3">eduardochiaro.com</span>
+      </div>
+      <div className="flex w-1/3">
+        <span className="grow text-right">Kernel</span>
+        <span className="mr-2">:</span>
+        <span className="text-zinc-100 w-2/3">5.0.0</span>
+      </div>
+      <div className="flex w-1/3">
+        <span className="grow text-right">Shell</span>
+        <span className="mr-2">:</span>
+        <span className="text-zinc-100 w-2/3">Terminal</span>
+      </div>
+      <div className="mt-4 mb-6">
+        color scheme:
+        <div className="flex gap-1">
+          <span class="flex-none w-6 h-6 bg-primary-500 border border-primary-400"></span>
+          <span class="flex-none w-6 h-6 bg-accent-500 border border-accent-400"></span>
+          <span class="flex-none w-6 h-6 bg-red-500 border border-red-400"></span>
+          <span class="flex-none w-6 h-6 bg-emerald-500 border border-emerald-400"></span>
+          <span class="flex-none w-6 h-6 bg-blue-500 border border-blue-400"></span>
+          <span class="flex-none w-6 h-6 bg-zinc-900 border border-zinc-700"></span>
+          <span class="flex-none w-6 h-6 bg-zinc-100 border border-zinc-300"></span>
+        </div>
+      </div>
+    </div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,8 @@ const plugin = require('tailwindcss/plugin')
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/utils/**/*.{js,ts,jsx,tsx}"
   ],
   darkMode: "class", 
   theme: {
