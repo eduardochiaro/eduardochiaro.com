6ce22c943576a789c38cc9b64618f506be85627a
Eduardo Chiaro <e.chiaro@gmail.com>
2022-06-03 17:39:31 -0700

    Update Navlink.js

--- src/components/Navlink.js
+++ src/components/Navlink.js
@@ -6,15 +6,15 @@ export default function NavLink({ href, as, exact, activeClassName, className, t
   const { asPath } = useRouter();
 
   const child = React.Children.only(children);
+  const asPathInitial = type == "main" ?  "/" + asPath.split('#')[0].split('/')[1] : asPath;
 
   const [classInUse, setClassInUse] = useState(className);
 
   useEffect(() => {
-    const asPathInitial = type == "main" ?  "/" + asPath.split('#')[0].split('/')[1] : asPath;
     const isActive = href == asPath || as == asPath || href == asPathInitial || as == asPathInitial;
     const classNameUse = isActive ? activeClassName : className ;
     setClassInUse(classNameUse);
-  }, [asPath, href, as, activeClassName, className, type]);
+  }, [asPath, asPathInitial, href, as, activeClassName, className]);
 
   return (
     <Link href={href} as={as} {...props}>
