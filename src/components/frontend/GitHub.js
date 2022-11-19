7e4011797ad34650f7f322cb120b0d6b338b1e03
Eduardo Chiaro <e.chiaro@gmail.com>
2022-11-19 10:14:22 -0800

    V5.1.1 (#26)
    * add fallback image for missing icon in bookmarks
    
    * replace image function for github
    
    * remove user-select: none;

--- src/components/frontend/GitHub.js
+++ src/components/frontend/GitHub.js
@@ -17,36 +17,10 @@ export default function GitHub() {
 
   const cutReposene = data ? data.results.filter((x) => !x.isArchived).slice(0, 6) : [];
 
-  const getLanguageIcon = (language) => {
-    switch (language) {
-      case 'JavaScript':
-        return 'js';
-      case 'Swift':
-        return 'swift';
-      case 'TypeScript':
-        return 'node';
-      case 'CSS':
-        return 'css';
-      case 'SCSS':
-      case 'CSS3':
-        return 'css3';
-      case 'PHP':
-        return 'php';
-      case 'Shell':
-        return 'shell';
-      default:
-        return 'html';
-    }
-  };
-
   const LoadImage = ({ src, alt }) => {
     const pick = randomIntFromInterval(1, 4);
-
-    return src.includes('avatars.githubusercontent.com') ? (
-      <Image src={`/images/random/${pick}.jpg`} fill sizes="33vw" className="z-10 object-cover" alt={alt} />
-    ) : (
-      <Image src={src} fill sizes="33vw" className="z-10 object-cover" alt={alt} />
-    );
+    const replaceSrc = src.includes('githubusercontent.com') ? src : `/images/random/${pick}.jpg`; 
+    return <Image src={replaceSrc} fill sizes="33vw" className="z-10 object-cover" alt={alt} />;
   };
 
   const items = cutReposene.map((repo, index) => (
