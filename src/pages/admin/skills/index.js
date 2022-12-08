e43d7bc599e669e380ff47a5363c49a8b751b703
Eduardo Chiaro <e.chiaro@gmail.com>
2022-12-08 11:29:53 -0800

    skills admin view update

--- src/pages/admin/skills/index.js
+++ src/pages/admin/skills/index.js
@@ -8,10 +8,11 @@ import path from 'path';
 import SVG from 'react-inlinesvg';
 import AdminModal from '@/components/admin/Modal';
 import AdminWrapper from '@/components/admin/Wrapper';
-import Table from '@/components/admin/Table';
 import mergeObj from '@/utils/mergeObj';
 import useStaleSWR from '@/utils/staleSWR';
 import moment from 'moment';
+import List from '@/components/admin/List';
+import { CheckIcon, TrashIcon } from '@heroicons/react/24/solid';
 
 const AdminSkillsIndex = ({ formRef, images }) => {
   const { data: skills, error } = useStaleSWR('/api/portfolio/skills');
@@ -31,6 +32,7 @@ const AdminSkillsIndex = ({ formRef, images }) => {
   const [isOpenDelete, setIsOpenDelete] = useState(false);
   const [skill, setSkill] = useState(skillFormat);
   const [formError, setFormError] = useState(false);
+  const [formSuccess, setFormSuccess] = useState(false);
 
   const onSubmitModal = async (e) => {
     e.preventDefault();
@@ -58,10 +60,19 @@ const AdminSkillsIndex = ({ formRef, images }) => {
       headers: {
         'Content-Type': 'application/json',
       },
-    }).then(({ data }) => {
-      mutate('/api/portfolio/skills');
-      closeModal();
-    });
+    })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        setFormError(true);
+        setFormSuccess(false);
+      })
+      .then(({ data }) => {
+        mutate('/api/portfolio/skills');
+        const mergedData = mergeObj(skillFormat, data);
+        setSkill(mergedData);
+        closeModal();
+      });
   };
 
   const isFormValid = (form) => {
@@ -92,12 +103,12 @@ const AdminSkillsIndex = ({ formRef, images }) => {
     const openSkill = mergeObj(skillFormat, skill);
     setSkill(openSkill);
     setIsOpen(true);
+    setFormSuccess(false);
   };
 
   const closeModal = () => {
-    setSkill(skillFormat);
-    setIsOpen(false);
     setFormError(false);
+    setFormSuccess(true);
   };
 
   const openModalDelete = (skill) => {
@@ -119,174 +130,156 @@ const AdminSkillsIndex = ({ formRef, images }) => {
     }
   };
 
-  const columns = [
-    {
-      name: 'Name',
-      key: 'name',
-      searchable: true,
-      classNameTd: 'font-bold',
-    },
-    {
-      name: 'Logo',
-      key: 'logo_d',
-      className: 'textcenter w-10',
-      classNameTd: 'text-center',
-    },
-    {
-      name: 'Percentage',
-      key: 'percentage_d',
-      searchable: true,
-      className: 'textcenter',
-      classNameTd: 'text-center',
-    },
-    {
-      name: 'Type',
-      key: 'type',
-      searchable: true,
-    },
-    {
-      name: 'Updated',
-      key: 'updated',
-      classNameTd: 'w-44',
-    },
-  ];
+  const columns = ['name', 'description', 'category_d'];
 
   const newData = [];
   skills?.results.map((item) => {
     const obj = { ...item };
+    obj.category_d = obj.type;
     obj.updated = moment(item.updatedAt || item.createdAt).from(moment());
-    obj.logo_d = (
-      <>
-        <div className={'w-32 m-auto relative'}>
-          <SVG alt={item.name} className={'inline w-auto fill-primary-700 dark:fill-primary-200'} src={`/images/svg-icons/${item.logo}`} height={50} />
-        </div>
-        <div className="small">{item.logo}</div>
-      </>
+    obj.image_d = (
+      <SVG alt={item.name} className={'object-cover w-full fill-primary-700 dark:fill-primary-200'} src={`/images/svg-icons/${item.logo}`} height={50} />
     );
-    obj.percentage_d = item.percentage + '%';
+    obj.description = item.percentage + '%';
     newData.push(obj);
   });
+
+  const title = (
+    <h1 className="grow flex items-center gap-2">
+      <CommandLineIcon className="h-6 text-secondary-700 dark:text-secondary-600" />
+      <span>Skills list</span>
+    </h1>
+  );
 
   if (session) {
     return (
       <AdminWrapper>
-        <AdminWrapper.Header>
-          <h1 className="text-2xl flex items-center gap-2">
-            <CommandLineIcon className="h-6 text-secondary-700 dark:text-secondary-600" /> Skills list
-          </h1>
-        </AdminWrapper.Header>
-        <Table
-          columns={columns}
-          data={newData}
-          format={skillFormat}
-          editAction={openModal}
-          deleteAction={openModalDelete}
-          openAction={openModal}
-          openActionLabel="Add new skill"
-        />
-        <AdminModal
-          title={skill.id ? 'Edit skill' : 'Add new skill'}
-          isOpen={isOpen}
-          closeModal={closeModal}
-          showButtons={true}
-          onSecondaryButtonClick={closeModal}
-          onPrimaryButtonClick={onPrimaryButtonClick}
-        >
-          <form ref={formRef} acceptCharset="UTF-8" method="POST" encType="multipart/form-data" onSubmit={onSubmitModal}>
-            {formError && (
-              <div className="bg-accent-100 border border-accent-400 text-accent-700 px-4 py-3 rounded relative mb-4" role="alert">
-                <strong className="font-bold">
-                  <ExclamationTriangleIcon className="inline-flex align-middle h-6 mr-4" />
-                  Invalid Form!{' '}
-                </strong>
-                <span className="block sm:inline">Some required fields are missing.</span>
-              </div>
-            )}
-            <div className="grid grid-cols-6 gap-6">
-              <div className="col-span-6">
-                <label htmlFor="name-form" className="input-label">
-                  Title <span className="text-secondary-700">*</span>
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="name-form"
-                  autoComplete="off"
-                  data-lpignore="true"
-                  data-form-type="other"
-                  className="mt-1 input-field"
-                  value={skill.name}
-                  onChange={handleChange}
-                  maxLength={191}
-                  required
-                />
-              </div>
-              <div className="col-span-5 sm:col-span-2">
-                <label htmlFor="logo-type-form" className="input-label">
-                  Logo <span className="text-secondary-700">*</span>
-                </label>
-                <select name="logo" id="logo-type-form" className="mt-1 input-field" onChange={handleChange} value={skill.logo} required>
-                  <option value="">Select logo</option>
-                  {images.map((item) => (
-                    <option key={item} value={item}>
-                      {item}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="col-span-1 relative">
-                <label htmlFor="logo-url-form" className="input-label">
-                  Preview
-                </label>
-                {skill.logo && (
-                  <SVG alt={skill.name} className={'inline-block w-14 fill-primary-700 dark:fill-primary-200'} src={`/images/svg-icons/${skill.logo}`} />
-                )}
-              </div>
-              <div className="col-span-6 sm:col-span-3">
-                <label htmlFor="percentage-form" className="input-label">
-                  Percentage <span className="text-secondary-700">*</span>
-                </label>
-                <input
-                  type="range"
-                  name="percentage"
-                  id="percentage-form"
-                  min="0"
-                  max="100"
-                  step="5"
-                  className="mt-1 range-field"
-                  value={skill.percentage}
-                  onChange={handleChange}
-                  required
-                />
-                <p className="text-sm">(selected: {skill.percentage}%)</p>
-              </div>
-              <div className="col-span-6">
-                <label htmlFor="type-form" className="input-label">
-                  Type <span className="text-secondary-700">*</span>
-                </label>
-                <input
-                  type="text"
-                  name="type"
-                  id="type-form"
-                  autoComplete="off"
-                  data-lpignore="true"
-                  data-form-type="other"
-                  className="mt-1 input-field"
-                  value={skill.type}
-                  onChange={handleChange}
-                  maxLength={191}
-                  required
-                />
-              </div>
+        <div className="h-full py-8 w-full w-1/4">
+          <List title={title} columns={columns} data={newData} format={skillFormat} openAction={openModal} editAction={openModal} activeId={skill.id} />
+        </div>
+        <div className={`bg-primary-50 dark:bg-primary-900 grow py-8 px-6 ${isOpen ? '' : 'hidden'}`}>
+          <div className="flex items-center justify-between">
+            <h2 className="text-2xl flex items-center gap-2">
+              <CommandLineIcon className="h-6 text-secondary-700 dark:text-secondary-600" /> {skill.id ? 'Edit skill' : 'Add new skill'}
+            </h2>
+            <div className="flex items-center gap-4">
+              <a href="#" className="text-sm text-red-500 font-semibold hover:underline" onClick={() => openModalDelete(skill)} role="menuitem" tabIndex="-1">
+                <TrashIcon className="inline-flex align-text-bottom h-4 mr-1" />
+                Delete
+              </a>
+              <button onClick={onPrimaryButtonClick} type="button" className={'button-success'}>
+                Save
+              </button>
             </div>
-          </form>
-        </AdminModal>
+          </div>
+
+          <div className={'mt-8 mb-2'}>
+            <form ref={formRef} acceptCharset="UTF-8" method="POST" encType="multipart/form-data" onSubmit={onSubmitModal}>
+              {formError && (
+                <div className="bg-accent-100 border border-accent-400 text-accent-700 px-4 py-3 rounded relative mb-4" role="alert">
+                  <strong className="font-bold">
+                    <ExclamationTriangleIcon className="inline-flex align-middle h-6 mr-4" />
+                    Invalid Form!{' '}
+                  </strong>
+                  <span className="block sm:inline">Some required fields are missing.</span>
+                </div>
+              )}
+              {formSuccess && (
+                <div className="bg-emerald-100 border border-emerald-400 text-emerald-700 px-4 py-3 rounded relative mb-4" role="alert">
+                  <strong className="font-bold">
+                    <CheckIcon className="inline-flex align-middle h-6 mr-4" />
+                    Success!{' '}
+                  </strong>
+                  <span className="block sm:inline">This page was saved.</span>
+                </div>
+              )}
+              <div className="grid grid-cols-6 gap-6">
+                <div className="col-span-6">
+                  <label htmlFor="name-form" className="input-label">
+                    Title <span className="text-secondary-700">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="name"
+                    id="name-form"
+                    autoComplete="off"
+                    data-lpignore="true"
+                    data-form-type="other"
+                    className="mt-1 input-field"
+                    value={skill.name}
+                    onChange={handleChange}
+                    maxLength={191}
+                    required
+                  />
+                </div>
+                <div className="col-span-5 sm:col-span-2">
+                  <label htmlFor="logo-type-form" className="input-label">
+                    Logo <span className="text-secondary-700">*</span>
+                  </label>
+                  <select name="logo" id="logo-type-form" className="mt-1 input-field" onChange={handleChange} value={skill.logo} required>
+                    <option value="">Select logo</option>
+                    {images.map((item) => (
+                      <option key={item} value={item}>
+                        {item}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="col-span-1 relative">
+                  <label htmlFor="logo-url-form" className="input-label">
+                    Preview
+                  </label>
+                  {skill.logo && (
+                    <SVG alt={skill.name} className={'inline-block w-14 fill-primary-700 dark:fill-primary-200'} src={`/images/svg-icons/${skill.logo}`} />
+                  )}
+                </div>
+                <div className="col-span-6 sm:col-span-3">
+                  <label htmlFor="percentage-form" className="input-label">
+                    Percentage <span className="text-secondary-700">*</span>
+                  </label>
+                  <input
+                    type="range"
+                    name="percentage"
+                    id="percentage-form"
+                    min="0"
+                    max="100"
+                    step="5"
+                    className="mt-1 range-field"
+                    value={skill.percentage}
+                    onChange={handleChange}
+                    required
+                  />
+                  <p className="text-sm">(selected: {skill.percentage}%)</p>
+                </div>
+                <div className="col-span-6">
+                  <label htmlFor="type-form" className="input-label">
+                    Type <span className="text-secondary-700">*</span>
+                  </label>
+                  <input
+                    type="text"
+                    name="type"
+                    id="type-form"
+                    autoComplete="off"
+                    data-lpignore="true"
+                    data-form-type="other"
+                    className="mt-1 input-field"
+                    value={skill.type}
+                    onChange={handleChange}
+                    maxLength={191}
+                    required
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
         <AdminModal
           title="Delete skill"
           isOpen={isOpenDelete}
-          closeModal={closeModalDelete}
+          closeModal={() => setIsOpenDelete(false)}
           showButtons={true}
-          onSecondaryButtonClick={closeModalDelete}
+          onSecondaryButtonClick={() => setIsOpenDelete(false)}
           onPrimaryButtonClick={onPrimaryButtonClickDelete}
           primaryButtonLabel="Delete"
           primaryButtonClass="button-danger"
