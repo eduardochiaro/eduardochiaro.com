3fcc727f2abf4d56bc1d03299db9f4647ea78ab7
Eduardo Chiaro <e.chiaro@gmail.com>
2023-07-29 18:03:46 -0700

    remove unused functions

--- src/utils/mergeObj.ts
+++
@@ -1,8 +0,0 @@
-export default function mergeObj(a: any, b: any) {
-  for (var key in b) {
-    if (key in a) {
-      a[key] = typeof a[key] === 'object' && typeof b[key] === 'object' ? mergeObj(a[key], b[key]) : b[key];
-    }
-  }
-  return a;
-}
