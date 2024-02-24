39858ea02d22df392065bac4ee0672435e42753d
Eduardo Chiaro <e.chiaro@gmail.com>
2024-02-23 19:40:04 -0800

    Update ShowProviders.tsx

--- src/components/ShowProviders.tsx
+++ src/components/ShowProviders.tsx
@@ -18,13 +18,13 @@ const LogoProvider = ({ id }: { id: string }) => {
 const ShowProviders = async function ShowProviders() {
   const providers = await getProviders();
   return (
-    <div className="flex flex-col gap-4">
+    <div className="flex flex-col gap-6 py-4">
       {providers &&
         Object.values(providers).map((provider) => (
-          <div key={provider.name} className="py-4">
+          <div key={provider.name}>
             <button
               onClick={() => signIn(provider.id)}
-              className="mx-auto flex items-center gap-3 rounded bg-primary-300 p-3 px-4 text-xl text-primary-700 ring-primary-300 ring-offset-2 ring-offset-primary-100 drop-shadow transition duration-300 ease-in-out hover:ring-2 dark:bg-primary-800 dark:text-primary-50 dark:ring-primary-700 dark:ring-offset-primary-600"
+              className="mx-auto flex items-center gap-3 rounded-md bg-primary-300 p-3 px-4 text-xl text-primary-700 ring-primary-300 ring-offset-2 ring-offset-primary-100 drop-shadow transition duration-300 ease-in-out hover:ring-2 dark:bg-primary-900 dark:text-primary-50 dark:ring-primary-700 dark:ring-offset-primary-600"
             >
               <LogoProvider id={provider.id} />
               Sign in with {provider.name}
