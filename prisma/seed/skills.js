25f83839dec08cb3693c4e7346f40ed3e824d878
Eduardo Chiaro <e.chiaro@gmail.com>
2024-04-10 08:01:39 -0700

    skill populate

--- prisma/seed/skills.js
+++ prisma/seed/skills.js
@@ -3,46 +3,46 @@ const prisma = new PrismaClient();
 
 const skills = [
   {
-    id: 1,
     name: 'Node.js',
     logo: 'node.svg',
     type: 'node',
     percentage: 85,
   },
   {
-    id: 2,
     name: 'HTML5',
     logo: 'html5.svg',
     type: 'html',
     percentage: 100,
   },
   {
-    id: 3,
     name: 'CSS3',
     logo: 'css3.svg',
     type: 'css',
     percentage: 85,
   },
   {
-    id: 4,
     name: 'PHP',
     logo: 'php.svg',
     type: 'php',
     percentage: 95,
   },
   {
-    id: 5,
     name: 'SQL',
     logo: 'mysql.svg',
     type: 'mysql',
     percentage: 80,
   },
   {
-    id: 6,
     name: 'AWS',
     logo: 'aws.svg',
     type: 'aws',
     percentage: 75,
+  },
+  {
+    name: 'React',
+    logo: 'react.svg',
+    type: 'react',
+    percentage: 90,
   },
 ];
 
