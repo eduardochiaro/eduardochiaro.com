a5cfaa00054858c26f114cb2b9e816f9faf82fd5
Eduardo Chiaro <e.chiaro@gmail.com>
2024-04-20 15:18:44 -0700

    bookcase style

--- src/components/frontend/Books.tsx
+++ src/components/frontend/Books.tsx
@@ -3,8 +3,9 @@
 import classNames from '@/utils/classNames';
 import { Prisma } from '@prisma/client';
 import styles from '@/styles/Book.module.scss';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import WireContainer from '../WireContainer';
+import SVG from '@/utils/svg';
 
 type BookWithTags = Prisma.BookGetPayload<{
   include: {
@@ -19,8 +20,18 @@ const randomHeight = () => {
   return height;
 };
 
+const chunkSize = 12;
+
+const chunk = (arr: any[]) =>
+  // Use Array.from to create a new array with a length equal to the number of chunks needed
+  Array.from({ length: Math.ceil(arr.length / chunkSize) }, (v, i) =>
+    // For each element in the new array, slice the original array to extract a chunk of 'size' elements
+    arr.slice(i * chunkSize, i * chunkSize + chunkSize),
+  );
+
 export default function Books({ data }: { data: BookWithTags[] }) {
   const [results, setResults] = useState(data);
+  const [chunks, setChunks] = useState<any[]>([]);
   const [selectedTag, setSelectedTag] = useState<any>(null);
 
   //get unique tags with count from books data as object in array
@@ -35,6 +46,10 @@ export default function Books({ data }: { data: BookWithTags[] }) {
     });
     return acc;
   }, []);
+
+  useEffect(() => {
+    setChunks(chunk(results));
+  }, [results]);
 
   const handleTagClick = (tag: any) => {
     if (tag.id === undefined) {
@@ -53,18 +68,19 @@ export default function Books({ data }: { data: BookWithTags[] }) {
 
   return (
     <section className={'mt-10 px-4 lg:px-0'}>
-      <div className="mx-auto flex max-w-5xl flex-wrap">
-        <div className="mb-10 basis-full md:basis-3/4">
-          <h1 className="h-10 font-header text-3xl font-light leading-tight tracking-wide lg:text-4xl">
-            Books I&apos;ve <span className="overlay-color">read</span>...
-          </h1>
-          <div className="mx-auto mt-10">
-            <div className="flex flex-col">
-              <div className="mx-4 flex h-64 items-end gap-0">
-                {results.map((book: BookWithTags, index: number) => (
+      <h1 className="mx-auto h-10 max-w-5xl font-header text-3xl font-light leading-tight tracking-wide lg:text-4xl">
+        Books I&apos;ve <span className="overlay-color">read</span>...
+      </h1>
+      <div className="mx-auto flex max-w-5xl">
+        <div className="mb-10 w-full md:w-3/4">
+          {chunks.map((chunk: BookWithTags[], index: number) => (
+            <div className="flex flex-col" key={`chunk-${index}`}>
+              <div className="mx-10 flex items-end justify-center gap-0 pt-5">
+                <SVG src="/images/svg-icons/side-bookcase-1.svg" width={100} />
+                {chunk.map((book: BookWithTags, index: number) => (
                   <div key={index} className="group">
                     <div
-                      className="w-10 overflow-hidden rounded-sm border-l-2 border-r-2 border-primary-800 bg-cyan-600 ring-1 ring-primary-700 group-hover:hidden"
+                      className="w-10 overflow-hidden rounded-t border-l-2 border-r-2 border-primary-800 bg-cyan-600 ring-1 ring-primary-700 group-hover:hidden"
                       style={{
                         minHeight: randomHeight(),
                         writingMode: 'vertical-lr',
@@ -90,17 +106,19 @@ export default function Books({ data }: { data: BookWithTags[] }) {
                     </div>
                   </div>
                 ))}
+                <SVG src="/images/svg-icons/side-bookcase-2.svg" width={100} />
               </div>
+
               <div className="h-2 rounded-lg bg-amber-950 drop-shadow-md"></div>
               <div className="mx-auto flex w-1/2 justify-between">
                 <div className="size-3 rounded-b-full bg-primary-950 drop-shadow-md"></div>
                 <div className="size-3 rounded-b-full bg-primary-950 drop-shadow-md"></div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
-        <div className="mb-10 basis-full md:basis-1/4 md:text-right">
-          <h2 className="mb-8 flex h-10 align-bottom font-header text-xl font-light leading-tight tracking-wide md:flex-row-reverse">
+        <div className="mb-10 w-full md:w-1/4 md:text-right">
+          <h2 className="mb-4 flex h-10 align-bottom font-header text-xl font-light leading-tight tracking-wide md:flex-row-reverse">
             <span className="self-end">Tags</span>
           </h2>
           {tags.length > 0 && (
